14ac47a208702a87c2634b7f1e348f7406892c2a
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-11-03 12:28:52 +0300

    Create dialogbox functions basic and update HTML

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import './style.css';
 import plusIcon from './plus-circle.png';
-import dialogPolyfill from 'dialog-polyfill'
 
 import {setDialogAsProject, setDialogAsTask, setDialogAsNote} from './modules/dialogBox.js';
 
+// dialog screen objects
+const dialog = document.querySelector('dialog');
 const addButton = document.querySelector("#add-project");
+const closeButton = document.querySelector('#exit-btn');
 
-addButton.addEventListener("click", setDialogAsProject);
+addButton.addEventListener('click', () => {
+    dialog.showModal();
+    setDialogAsProject();
+});
+
+closeButton.addEventListener('click', () => {
+    dialog.close();
+})
--- src/modules/dialogBox.js
+++ src/modules/dialogBox.js
@@ -1,14 +1,17 @@
 
 function setDialogAsProject() {
-    
+    const header = document.querySelector(".dialog-header-text");
+    header.innerHTML = "Create a new project";
 }
 
 function setDialogAsTask() {
-
+    const header = document.querySelector(".dialog-header-text");
+    header.innerHTML = "Create a new task";
 }
 
 function setDialogAsNote() {
-
+    const header = document.querySelector(".dialog-header-text");
+    header.innerHTML = "Create a new note";
 }
 
 export {
