a971d75b07a55f3fe4fe75c56f91ba216b720843
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-11-22 01:52:08 +0300

    Add todo class

---
+++ src/modules/todo.js
@@ -0,0 +1,31 @@
+export default class todo {
+  constructor(title, details, priority) {
+    this.title = title;
+    this.details = details;
+    this.priority = priority;
+  }
+
+  setTitle(title) {
+    this.title = title;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  setDetails(details) {
+    this.details = details;
+  }
+
+  getDetails() {
+    return this.details;
+  }
+
+  setPriority(priority) {
+    this.priority = priority;
+  }
+
+  getPriority() {
+    return this.priority;
+  }
+}
